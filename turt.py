090f025a023389363be7ce08f1d98b58bb7c1a09
mohammad <moaziz.me@outlook.com>
2022-04-24 20:50:13 +1000

    working with turtle

---
+++ turt.py
@@ -0,0 +1,19 @@
+import turtle
+
+turtle.speed(0)
+turtle.bgcolor("black")
+for i in range (15):
+    for colors in ("red", "green", "blue", "magenta", "yellow", "white"):
+        turtle.color(colors)
+        turtle.pensize(3)
+        turtle.left(4)
+        turtle.forward(200)
+        turtle.left(90)
+        turtle.forward(200)
+        turtle.left(90)
+        turtle.forward(200)
+        turtle.left(90)
+        turtle.forward(200)
+        turtle.left(90)
+
+
